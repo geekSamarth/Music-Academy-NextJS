ffdce9f0ae84445b4afd96d7398cecb46feff35e
geeksamarth <samarthagrawal1804@gmail.com>
2024-05-20 17:10:33 +0530

    chore: Update npm dependencies and add new components to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialsCards from "@/components/TestimonialsCards";
+import WhyChoseUs from "@/components/WhyChoseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main
+      className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] 
+    "
+    >
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChoseUs/>
+      <TestimonialsCards />
     </main>
   );
 }
