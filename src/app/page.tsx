9511457536f44fdebb5229e2ad64b0880b19df74
geeksamarth <samarthagrawal1804@gmail.com>
2024-05-21 17:28:11 +0530

    Add few more compoents and links in the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialsCards from "@/components/TestimonialsCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChoseUs from "@/components/WhyChoseUs";
 
 export default function Home() {
@@ -13,6 +16,9 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChoseUs/>
       <TestimonialsCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
